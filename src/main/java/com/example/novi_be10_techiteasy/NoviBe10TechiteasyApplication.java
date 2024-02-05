44d4063360cd5b6f6de4cd93597edf00c62746fe
szuiderweg <svmzuiderweg@hotmail.com>
2024-02-05 14:00:24 +0100

    eerste opzet met Spring Boot

---
+++ src/main/java/com/example/novi_be10_techiteasy/NoviBe10TechiteasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.novi_be10_techiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviBe10TechiteasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoviBe10TechiteasyApplication.class, args);
+	}
+
+}
